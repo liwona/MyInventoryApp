e086ebddf684c6e0d159b120756c294ff4461886
liwona <liwona@users.noreply.github.com>
2018-06-27 23:18:14 +0200

    MyInventoryApp initial commit

---
+++ app/src/main/java/com/example/android/myinventoryapp/data/InventoryDbHelper.java
@@ -0,0 +1,4 @@
+package com.example.android.myinventoryapp.data;
+
+public class InventoryDbHelper {
+}
---
+++ app/src/main/java/com/example/android/myinventoryapp/data/InventoryProvider.java
@@ -0,0 +1,8 @@
+package com.example.android.myinventoryapp.data;
+
+/**
+ * Created by Iwona on 27.06.2018.
+ */
+
+public class InventoryProvider {
+}
