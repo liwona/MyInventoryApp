6d40c0c958bb42e8d87fd79d1948689141f9ec57
liwona <liwona@users.noreply.github.com>
2018-06-28 23:23:47 +0200

    Loader added to be able to update the Views and show data

--- app/src/main/java/com/example/android/myinventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/myinventoryapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.myinventoryapp;
 
 import android.app.AlertDialog;
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -17,7 +21,7 @@ import android.widget.ListView;
 
 import com.example.android.myinventoryapp.data.InventoryContract.InventoryEntry;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
 
     // Identifies a particular Loader being used in this component
@@ -71,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 //        // Kick off the loader
-//        getLoaderManager().initLoader(PET_LOADER, null, this);
+        getLoaderManager().initLoader(PET_LOADER, null, this);
 
     }
 
@@ -153,5 +157,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void deleteAllBooks() {
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // Define a projection that specifies the columns from the table we care about
+        String[] projection = {
+                InventoryEntry._ID,
+                InventoryEntry.COLUMN_BOOK_NAME,
+                InventoryEntry.COLUMN_BOOK_PRICE,
+                InventoryEntry.COLUMN_BOOK_QUANTITY
+        };
+
+        // This Loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                InventoryEntry.CONTENT_URI,        // Table to query
+                projection,     // Projection to return
+                null,            // No selection clause
+                null,            // No selection arguments
+                null             // Default sort order
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Update {@Link InventoryCursorAdapter} with this new cursor containing updated pet data
+        mCursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
     }
 }
