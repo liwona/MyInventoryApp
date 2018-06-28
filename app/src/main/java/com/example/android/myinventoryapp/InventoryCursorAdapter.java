9b46ec946a95eda7d8411c7e5fef866d5a532c05
liwona <liwona@users.noreply.github.com>
2018-06-28 10:55:57 +0200

    InventoryCursorAdapter logic added

--- app/src/main/java/com/example/android/myinventoryapp/InventoryCursorAdapter.java
+++ app/src/main/java/com/example/android/myinventoryapp/InventoryCursorAdapter.java
@@ -1,8 +1,76 @@
 package com.example.android.myinventoryapp;
 
-/**
- * Created by Iwona on 28.06.2018.
- */
 
-public class InventoryCursorAdapter {
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.myinventoryapp.data.InventoryContract.InventoryEntry;
+
+/**
+ * {@link InventoryCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of pet data as its data source. This adapter knows
+ * how to create list items for each row of pet data in the {@link Cursor}.
+ */
+public class InventoryCursorAdapter extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link InventoryCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public InventoryCursorAdapter(Context context, Cursor c) {
+        super(context, c);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the pet data (in the current row pointed to by cursor) to the given
+     * list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find fields to populate in inflated template
+        TextView nameTextVIew = view.findViewById(R.id.name);
+        TextView priceTextView = view.findViewById(R.id.price);
+        TextView quantityTextView = view.findViewById(R.id.quantity);
+
+        // Find the columns of the book attributes that we're interested in
+        int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_BOOK_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_BOOK_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_BOOK_QUANTITY);
+
+        // Read the book attributes from the Cursor for the current book
+        String bookName = cursor.getString(nameColumnIndex);
+        String bookPrice = cursor.getString(priceColumnIndex);
+        String bookQuantity = cursor.getString(quantityColumnIndex);
+
+        // Populate fields with extracted properties
+        nameTextVIew.setText(bookName);
+        priceTextView.setText(bookPrice);
+        quantityTextView.setText(bookQuantity);
+    }
 }
--- app/src/main/java/com/example/android/myinventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/myinventoryapp/MainActivity.java
@@ -1,29 +1,25 @@
 package com.example.android.myinventoryapp;
 
+import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.example.android.myinventoryapp.data.InventoryContract.InventoryEntry;
 
 public class MainActivity extends AppCompatActivity {
 
-    /** EditText field to enter the book's name */
-    private EditText mNameEditText;
 
-    /** EditText field to enter price*/
-    private EditText mPriceEditText;
+    // Identifies a particular Loader being used in this component
+    private static final int PET_LOADER = 0;
 
-    /** Spiiner field to enter quantity*/
-    private EditText mQuantitySpinner;
-
-    /** EditText field to enter supplier name*/
-    private EditText mSupplierName;
-
-    /** EditText field to enter supplier phone number*/
-    private EditText mSupplierPhone;
+    InventoryCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +39,35 @@ public class MainActivity extends AppCompatActivity {
         // Find the ListView which will be populated with the pet data
         ListView inventoryListView = (ListView) findViewById(R.id.inventory_list_view);
 
+        // Setup the Adapter to create a list item view for each row of pet data in the Cursor.
+        // There is no pet data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new InventoryCursorAdapter(this, null);
+        inventoryListView.setAdapter(mCursorAdapter);
 
+        // Setup item click listener
+        inventoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Create new intent to go to {@Link EditActivity}
+                Intent intent = new Intent(MainActivity.this, EditFragment.class);
+
+                // Form the content URI that represents the specific book that was clicked on,
+                // by appending the "id" (passed as input to this method) onto the
+                // {@link InventoryEntry#CONTENT_URI}.
+                // For example, the URI would be "content://com.example.android.books/books/2"
+                // if the book with ID 2 was clicked on.
+                Uri currentBookUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentBookUri);
+
+                // Launch the {@link EditorActivity} to display the data for the current pet.
+                startActivity(intent);
+            }
+        });
+
+//        // Kick off the loader
+//        getLoaderManager().initLoader(PET_LOADER, null, this);
 
     }
 }
