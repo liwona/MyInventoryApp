2f9cffd7d03c6f2d12ac9a6e1354c4f707d08c32
liwona <liwona@users.noreply.github.com>
2018-07-02 03:14:12 +0200

    show dialog for unsaved changes

--- app/src/main/java/com/example/android/myinventoryapp/EditActivity.java
+++ app/src/main/java/com/example/android/myinventoryapp/EditActivity.java
@@ -351,6 +351,24 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
 
 
     private void showUnsavedChangesDialog(DialogInterface.OnClickListener discardButtonClickListener) {
+            // Create an AlertDialog.Builder and set the message, and click listeners
+            // for the positive and negative buttons on the dialog.
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.unsaved_changes_msg);
+            builder.setPositiveButton(R.string.discard_changes, discardButtonClickListener);
+            builder.setNegativeButton(R.string.keep_editing_book, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User clicked the "Keep editing" button, so dismiss the dialog
+                    // and continue editing the pet.
+                    if (dialog != null) {
+                        dialog.dismiss();
+                    }
+                }
+            });
+
+            // Create and show the AlertDialog
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
     }
 
     private void showDeleteConfirmationDialog() {
